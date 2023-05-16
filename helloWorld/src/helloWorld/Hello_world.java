dd41d978b4fc9f4e37dde7810924bdf44f5c3d53
Matthew Re <69985820+Matthewjre@users.noreply.github.com>
2023-05-16 07:08:40 -0400

    Update Hello_world.java

--- helloWorld/src/helloWorld/Hello_world.java
+++ helloWorld/src/helloWorld/Hello_world.java
@@ -4,7 +4,7 @@ public class Hello_world {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
+		System.out.println("Bonjour le monde");
 		//Added changes to pull
 	}
 
